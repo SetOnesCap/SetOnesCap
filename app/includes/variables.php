d3ee7bf28a4ede904c81b02c611f6183cdbc8438
Benjamin Dehli <superelg@gmail.com>
2014-12-25 01:26:52 +0100

    Adds string replace from dash to space

--- app/includes/variables.php
+++ app/includes/variables.php
@@ -2,6 +2,7 @@
 $db = new DataBase();
 $siteName = "Set One's Cap";
 $pageTitle = isset($_GET['pageTitle']) ? $_GET['pageTitle'] : 'Home';
+$pageTitle = str_replace('-', ' ', $pageTitle);
 $pageId = __getSingleValue($db::PAGE_IDCOL, $db::PAGE_TABLENAME, $db::PAGE_TITLECOL, $pageTitle);
 $pageFile = __getPageFile($pageId);
 
