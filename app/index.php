7e071292972bb5e2ed600f22c119dc3e0cb6d18a
Benjamin Dehli <superelg@gmail.com>
2015-01-11 13:02:15 +0100

    Add admin page redirect

--- app/index.php
+++ app/index.php
@@ -1,13 +1,18 @@
 <?php
+header('Content-Type: text/html; charset=UTF-8');
 
 
 
-include("service-variables.php");
-include("modules/functions.php");
-include("includes/variables.php");
-include("includes/head.php");
+include("./service-variables.php");
+include("./modules/functions.php");
+include("./includes/variables.php");
+include("./includes/head.php");
 
-if($pageTitle == 'photos' && $photoAlbum != ''){
+if($pageTitle == 'admin'){
+    header("Location: admin/index.php");
+    die();
+   // include("admin/includes/admin-template.php");
+}else if($pageTitle == 'photos' && $photoAlbum != ''){
     include("includes/photo-template.php");
 }else {
     include("includes/template.php");
