e0e9cf7c03edceec45420036b9b21fbdef6259d4
Benjamin Dehli <superelg@gmail.com>
2015-01-14 17:15:43 +0100

    Update photo page

--- public_html/content/photos.php
+++ public_html/content/photos.php
@@ -3,37 +3,22 @@
         <p><?php __getContent($pageId); ?></p>
     </div>
 </div>
+<form>
+    <div class="col-12 bg-white fg-black">
+        <h2>Photos of Set One's Cap (2014 - present)</h2>
+        <?php __getPhotoAlbums('setonescap') ?>
+        <div class='clear'></div>
+    </div>
 
-    <form>
-
-        <div class="col-12 bg-white fg-black">
-            <h2>Photos of Set One's Cap</h2>
-
-            <?php __getPhotoAlbums('setonescap') ?>
-
-            <div class='clear'></div>
-            </div>
-
-        <div class="col-12 bg-white fg-black">
-            <h2>Photos of Confusion</h2>
-
-            <?php __getPhotoAlbums('confusion') ?>
-
-            <div class='clear'></div>
-        </div>
-    </form>
-
-
+    <div class="col-12 bg-white fg-black">
+        <h2>Photos of Confusion (2011 - 2014)</h2>
+        <?php __getPhotoAlbums('confusion') ?>
+        <div class='clear'></div>
+    </div>
+</form>
 <div class='clear'></div>
-
-
-
-
-
 <div id="photoalbum" class="photoalbumDialog">
     <div id="preview">
         <a href='#close' title='Close' class='close'>X</a>
-
     </div>
-
 </div>
