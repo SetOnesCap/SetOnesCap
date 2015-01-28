e4e67df1f15209c6654258696c479d59dab88001
Benjamin Dehli <superelg@gmail.com>
2015-01-28 20:20:00 +0100

    Add sitemaps and 404-page

---
+++ public_html/404.php
@@ -0,0 +1,179 @@
+
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="Content-Language" content="en">
+    <meta name="description" content="Read about Set One's Cap and the member of the band">
+    <meta name="keywords" content="band, norwegian, pop, rock, pop-rock, music, concerts, photos, videos, info, biography">
+    <meta name="author" content="Benjamin Dehli">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+
+
+    <title>
+        404 - Set One's Cap    </title>
+
+    <!-- Add to homescreen for Chrome on Android -->
+    <meta name="mobile-web-app-capable" content="yes">
+    <link rel="icon" sizes="192x192" href="/images/touch/chrome-touch-icon-192x192.png">
+
+    <!-- Add to homescreen for Safari on iOS -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-title" content="Web Starter Kit">
+    <link rel="apple-touch-icon-precomposed" href="apple-touch-icon-precomposed.png">
+
+    <!-- Tile icon for Win8 (144x144 + tile color) -->
+    <meta name="msapplication-TileImage" content="/images/touch/ms-touch-icon-144x144-precomposed.png">
+    <meta name="msapplication-TileColor" content="#3372DF">
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" type="text/css" href="/font-awesome-4.2.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="/styles/standard.css">
+    <link rel="stylesheet" type="text/css" href="/styles/custom.css">
+    <link rel="stylesheet" href="/styles/slicknav.css">
+
+
+
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
+</head>
+<body class="bg-white"><script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-57441525-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+
+
+<div class="content">
+    <header>
+        <nav id="menu" class="fg-black bg-white fixed">
+            <img src="/images/template/bird-nav.png" alt="Set Ones Cap logo, drawing of a bird with a hat"/>
+            <ul class="float-right">
+                <li><a href='/'>Home</a></li><li><a href='/concerts/'>Concerts</a></li><li><a href='/photos/'>Photos</a></li><li><a href='/videos/'>Videos</a></li><li><a href='/about/'>About</a></li>            </ul>
+
+        </nav>
+    </header>
+    <section class="col-12 fg-black header fourohfour" data-speed="1.4" data-type="background">
+
+
+
+    </section>
+    <main>
+        
+
+<div class="col-12 bg-white fg-black">
+    <div class="col-8" id='introduction'>
+        <p>Sorry, this page does not exist.</p>
+        <p>If you were directed here from a link, please let us know by sending a email to <a href="mailto:post@setonescap.com?Subject=404%20Error" target="_top">post@setonescap.com</a></p>
+    </div>
+    <div class="col-4 text-center">
+        <img src="/images/template/logo-big.png" alt="Set Ones Cap logo, drawing of a bird with a hat"/>
+    </div>
+</div>    </main>
+
+    <div class="col-12 bg-noise bg-black fg-white bottom-col">
+        <div class="col-12 text-center sociallinks">
+            <h2>Get in touch</h2>
+            <a href="https://www.facebook.com/setonescap" class="facebook" title="Check out Set One's Cap at Facebook">
+                <span class="fa-stack fa-2x">
+                    <i class="fa fa-square-o fa-stack-2x"></i>
+                    <i class="fa fa-facebook fa-stack-1x"></i>
+                </span>
+                <p>Facebook</p>
+            </a>
+            <a href="https://twitter.com/setonescap" class="twitter" title="Check out Set One's Cap at Twitter">
+                <span class="fa-stack fa-2x">
+                    <i class="fa fa-square-o fa-stack-2x"></i>
+                    <i class="fa fa-twitter fa-stack-1x"></i>
+                </span>
+                <p>Twitter</p>
+            </a>
+            <a href="http://instagram.com/setonescap" class="instagram" title="Check out Set One's Cap at Instagram">
+                <span class="fa-stack fa-2x">
+                    <i class="fa fa-square-o fa-stack-2x"></i>
+                    <i class="fa fa-instagram fa-stack-1x"></i>
+                </span>
+                <p>Instagram</p>
+            </a>
+            <a href="https://www.youtube.com/user/setonescap" class="youtube" title="Check out Set One's Cap at YouTube">
+                <span class="fa-stack fa-2x">
+                    <i class="fa fa-square-o fa-stack-2x"></i>
+                    <i class="fa fa-youtube fa-stack-1x"></i>
+                </span>
+                <p>YouTube</p>
+            </a>
+            <a href="https://soundcloud.com/setonescap" class="soundcloud" title="Check out Set One's Cap at SoundCloud">
+                <span class="fa-stack fa-2x">
+                    <i class="fa fa-square-o fa-stack-2x"></i>
+                    <i class="fa fa-soundcloud fa-stack-1x"></i>
+                </span>
+                <p>Soundcloud</p>
+            </a>
+            <a href="https://plus.google.com/111095056778547378720" rel="publisher" class="google-plus" title="Check out Set One's Cap at Google Plus">
+                <span class="fa-stack fa-2x">
+                    <i class="fa fa-square-o fa-stack-2x"></i>
+                    <i class="fa fa-google-plus fa-stack-1x"></i>
+                </span>
+                <p>Google+</p>
+            </a>
+
+        </div>
+        <div itemscope itemtype="http://data-vocabulary.org/Person" class="col-6">
+            <h3>Web:</h3>
+            <p><span itemprop="name">Benjamin Dehli</span></p>
+            <div itemprop="address" itemscope itemtype="http://data-vocabulary.org/Address" class="no-padding">
+                <p><span itemprop="street-address">Margretes veg 15</span></p>
+                <p><span itemprop="postal-code">3804</span> <span itemprop="locality">B&oslash; i Telemark</span></p>
+                <p><span itemprop="country-name">Norway</span></p>
+            </div>
+            <p>Phone: +47 92 29 27 19</p>
+            <p>E-mail: post@setonescap.com</p>
+        </div>
+        <div itemscope itemtype="http://data-vocabulary.org/Person" class="col-6">
+            <h3>Booking:</h3>
+            <p><span itemprop="name">Carl-Viktor Guttormsen</span></p>
+            <p>Parkveien 1</p>
+            <p>3970 Langesund</p>
+            <p>Norway</p>
+            <p>Phone: +47 90 26 21 60</p>
+            <p>E-mail: booking@setonescap.com</p>
+        </div>
+
+
+    </div>
+    <footer class="fixed-bottom bg-white fg-black">
+        <div class="col-6">Copyright © 2014 <a href="https://plus.google.com/+Setonescap" target="_blank">Set One's Cap</a></div>
+        <div class="col-6 text-right">
+            Website by: <a href="https://plus.google.com/+BenjaminDehli1/" rel="publisher" target="_blank">Benjamin Dehli</a>
+        </div>
+    </footer>
+
+
+    <div class="clearfix"></div>
+</div>
+<script type="text/javascript" src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+
+<script type="text/javascript" src="/scripts/jquery.slicknav.js"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#menu').slicknav();
+    });
+</script>
+
+<script type="text/javascript" src="/scripts/showPhoto.js"></script>
+<script type="text/javascript" src="/scripts/parallax.js"></script>
+<script type="text/javascript" src="/scripts/activeLinks.js"></script>
+
+</body>
+</html>
+
