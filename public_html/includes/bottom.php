255b5272d7ea2abbe16b464a841b830a902dc24f
Benjamin Dehli <superelg@gmail.com>
2015-02-11 21:23:19 +0100

    Remove unused files

--- public_html/includes/bottom.php
+++
@@ -1,6 +0,0 @@
-<script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-<script type="text/javascript" src="scripts/parallax.js"></script>
-<div class="clearfix"></div>
-</div>
-</body>
-</html>
--- public_html/includes/navbar.php
+++
@@ -1,9 +0,0 @@
-<header>
-    <nav class="fg-white">
-        <ul>
-            <?php
-            __getLinkList($rootURL);
-            ?>
-        </ul>
-    </nav>
-</header>
