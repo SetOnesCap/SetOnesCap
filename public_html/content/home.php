8aa99c3a5ebd998f4371a32f32502a932e5b4692
Benjamin Dehli <superelg@gmail.com>
2015-02-16 22:57:41 +0100

    Delete unused code

--- public_html/content/home.php
+++ public_html/content/home.php
@@ -13,18 +13,4 @@ $i = 0;
 <div class="col-12 bg-white">
     <?php include_once('./services/facebook.php'); ?>
 </div>
-<!--
-<div class="col-12 bg-noise bg-black fg-white box-nav">
-    <?php foreach ($nyheter as $n => $nyhet) {
-        ?>
-        <a href="#">
-            <div class="col-4 bg-noise bg-<?php echo "setonescap-red" ?> box-nav-item" id="<?php echo $nyhet; ?>">
-                <div class="box-nav-link">
-                    <h2><?php echo $nyhet; ?></h2>
-                    <span>read more about <?php echo $nyhet; ?> here</span></div>
-                <div class="box-nav-hover"></div>
-            </div>
-        </a>
 
-        <?php $i+=1; } ?>
-</div>-->
