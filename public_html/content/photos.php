8d1ccd9c6f7bd59b54c7d382919e84fecd7a320f
Benjamin Dehli <superelg@gmail.com>
2015-03-12 11:50:28 +0100

    Remove unused code for Confusion photos

--- public_html/content/photos.php
+++ public_html/content/photos.php
@@ -9,12 +9,6 @@
         <?php __getPhotoAlbums('setonescap') ?>
         <div class='clear'></div>
     </div>
-
-    <!--<div class="col-12 bg-white fg-black">
-        <h2>Photos of Confusion (2011 - 2014)</h2>
-        <?php __getPhotoAlbums('confusion') ?>
-        <div class='clear'></div>
-    </div>-->
 </form>
 <div class='clear'></div>
 <div id="photoalbum" class="photoalbumDialog">
