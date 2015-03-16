93ae918e02190a6145b9ec56237642fe5401fac5
Benjamin Dehli <superelg@gmail.com>
2015-03-17 00:14:57 +0100

    Remove unused code

--- public_html/content/home.php
+++ public_html/content/home.php
@@ -1,10 +1,3 @@
-<?php
-$nyheter = array("Concerts","Info","Videos","Photos","Booking","Music");
-$farge = array("red", "yellow", "blue", "cyan", "magenta", "green");
-$i = 0;
-
-?>
-
 <div class="col-12 bg-white fg-black text-center">
     <div class="col-8 center" id='introduction'>
     <p><?php __getContent($pageId); ?></p>
@@ -12,6 +5,5 @@ $i = 0;
 </div>
 <div class="col-12 bg-white">
     <?php include_once('./services/facebook.php'); ?>
-   <?php// include_once('./services/instagram.php'); ?>
 </div>
 
