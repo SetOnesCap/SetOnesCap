3e7e12b16758c00e5849d87976a0eb3c65bbb8a6
Benjamin Dehli <superelg@gmail.com>
2015-04-29 22:48:53 +0200

    Add short info about members of the band

--- public_html/content/about.php
+++ public_html/content/about.php
@@ -2,7 +2,17 @@
 
 <div class="col-12 bg-white fg-black">
     <div class="col-8" id='introduction'>
-        <p><?php __getContent($pageId); ?></p>
+        <h2>Biography</h2>
+        <?php __getContent($pageId); ?>
+        <h3>Members</h3>
+        <ul>
+            <li>Joakim Juul - Vocals</li>
+            <li>Martin Torgersen - Synth and Piano</li>
+            <li>Benjamin Dehli - Organ and Electric piano</li>
+            <li>Carl-Viktor Guttormsen - Guitar</li>
+            <li>Sondre Gautefald - Bass</li>
+            <li>Kasper Waag - Drums</li>
+        </ul>
     </div>
     <div class="col-4 text-center">
         <img src="/images/template/logo-big.png" alt="Set Ones Cap logo, drawing of a bird with a hat"/>
