aa6744607cf28ecadcbbc6c81473044b8a3ff29d
Benjamin Dehli <superelg@gmail.com>
2015-09-20 15:29:45 +0200

    Update 404-page

--- public_html/404.php
+++ public_html/404.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
@@ -11,11 +10,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
 
-    <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+    <link rel="icon" type="image/x-icon" href="/favicon.ico"/>
 
 
     <title>
-        404 - Set One's Cap    </title>
+        404 - Set One's Cap </title>
 
     <!-- Add to homescreen for Chrome on Android -->
     <meta name="mobile-web-app-capable" content="yes">
@@ -38,20 +37,26 @@
     <link rel="stylesheet" href="/styles/slicknav.css">
 
 
-
     <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.min.js"></script>
+    <script>
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+        ga('create', 'UA-57441525-1', 'auto');
+        ga('send', 'pageview');
+
+    </script>
 </head>
-<body class="bg-white"><script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-57441525-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
-
+<body class="bg-white">
 
 
 <div class="content">
@@ -59,94 +64,140 @@
         <nav id="menu" class="fg-black bg-white fixed">
             <img src="/images/template/bird-nav.png" alt="Set Ones Cap logo, drawing of a bird with a hat"/>
             <ul class="float-right">
-                <li><a href='/'>Home</a></li><li><a href='/concerts/'>Concerts</a></li><li><a href='/photos/'>Photos</a></li><li><a href='/videos/'>Videos</a></li><li><a href='/about/'>About</a></li>            </ul>
+                <li><a href='/'>Home</a></li>
+                <li><a href='/concerts/'>Concerts</a></li>
+                <li><a href='/photos/'>Photos</a></li>
+                <li><a href='/music/'>Music</a></li>
+                <li><a href='/videos/'>Videos</a></li>
+                <li><a href='/about/'>About</a></li>
+            </ul>
 
         </nav>
     </header>
     <section class="col-12 fg-black header fourohfour" data-speed="1.4" data-type="background">
 
 
-
     </section>
     <main>
-        
 
-<div class="col-12 bg-white fg-black">
-    <div class="col-8" id='introduction'>
-        <p>Sorry, this page does not exist.</p>
-        <p>If you were directed here from a link, please let us know by sending a email to <a href="mailto:post@setonescap.com?Subject=404%20Error" target="_top">post@setonescap.com</a></p>
-    </div>
-    <div class="col-4 text-center">
-        <img src="/images/template/logo-big.png" alt="Set Ones Cap logo, drawing of a bird with a hat"/>
-    </div>
-</div>    </main>
+
+        <div class="col-12 bg-white fg-black">
+            <div class="col-8" id='introduction'>
+                <p>Sorry, this page does not exist.</p>
+
+                <p>If you were directed here from a link, please let us know by sending a email to
+                    <a href="mailto:post@setonescap.com?Subject=404%20Error" target="_top">post@setonescap.com</a></p>
+            </div>
+            <div class="col-4 text-center">
+                <img src="/images/template/logo-big.png" alt="Set Ones Cap logo, drawing of a bird with a hat"/>
+            </div>
+        </div>
+    </main>
 
     <div class="col-12 bg-noise bg-black fg-white bottom-col">
         <div class="col-12 text-center sociallinks">
-            <h2>Get in touch</h2>
-            <a href="https://www.facebook.com/setonescap" class="facebook" title="Check out Set One's Cap at Facebook">
-                <span class="fa-stack fa-2x">
-                    <i class="fa fa-square-o fa-stack-2x"></i>
-                    <i class="fa fa-facebook fa-stack-1x"></i>
-                </span>
+            <div class="line-through">
+                <span></span>
+
+                <h2>Get in touch</h2>
+            </div>
+
+            <a href="https://www.facebook.com/setonescap" target="_blank" class="facebook" title="Check out Set One's Cap at Facebook">
+                <div class="fa-stack fa-2x">
+                    <span class="fa fa-square-o fa-stack-2x"></span>
+                    <span class="fa fa-facebook fa-stack-1x"></span>
+                </div>
                 <p>Facebook</p>
             </a>
-            <a href="https://twitter.com/setonescap" class="twitter" title="Check out Set One's Cap at Twitter">
-                <span class="fa-stack fa-2x">
-                    <i class="fa fa-square-o fa-stack-2x"></i>
-                    <i class="fa fa-twitter fa-stack-1x"></i>
-                </span>
+            <a href="https://twitter.com/setonescap" target="_blank" class="twitter" title="Check out Set One's Cap at Twitter">
+                <div class="fa-stack fa-2x">
+                    <span class="fa fa-square-o fa-stack-2x"></span>
+                    <span class="fa fa-twitter fa-stack-1x"></span>
+                </div>
                 <p>Twitter</p>
             </a>
-            <a href="http://instagram.com/setonescap" class="instagram" title="Check out Set One's Cap at Instagram">
-                <span class="fa-stack fa-2x">
-                    <i class="fa fa-square-o fa-stack-2x"></i>
-                    <i class="fa fa-instagram fa-stack-1x"></i>
-                </span>
+            <a href="http://instagram.com/setonescap" target="_blank" class="instagram" title="Check out Set One's Cap at Instagram">
+                <div class="fa-stack fa-2x">
+                    <span class="fa fa-square-o fa-stack-2x"></span>
+                    <span class="fa fa-instagram fa-stack-1x"></span>
+                </div>
                 <p>Instagram</p>
             </a>
-            <a href="https://www.youtube.com/user/setonescap" class="youtube" title="Check out Set One's Cap at YouTube">
-                <span class="fa-stack fa-2x">
-                    <i class="fa fa-square-o fa-stack-2x"></i>
-                    <i class="fa fa-youtube fa-stack-1x"></i>
-                </span>
+            <a href="https://www.youtube.com/user/setonescap" target="_blank" class="youtube" title="Check out Set One's Cap at YouTube">
+                <div class="fa-stack fa-2x">
+                    <span class="fa fa-square-o fa-stack-2x"></span>
+                    <span class="fa fa-youtube fa-stack-1x"></span>
+                </div>
                 <p>YouTube</p>
             </a>
-            <a href="https://soundcloud.com/setonescap" class="soundcloud" title="Check out Set One's Cap at SoundCloud">
-                <span class="fa-stack fa-2x">
-                    <i class="fa fa-square-o fa-stack-2x"></i>
-                    <i class="fa fa-soundcloud fa-stack-1x"></i>
-                </span>
+            <a href="https://soundcloud.com/setonescap" target="_blank" class="soundcloud" title="Check out Set One's Cap at SoundCloud">
+                <div class="fa-stack fa-2x">
+                    <span class="fa fa-square-o fa-stack-2x"></span>
+                    <span class="fa fa-soundcloud fa-stack-1x"></span>
+                </div>
                 <p>Soundcloud</p>
             </a>
-            <a href="https://plus.google.com/111095056778547378720" rel="publisher" class="google-plus" title="Check out Set One's Cap at Google Plus">
-                <span class="fa-stack fa-2x">
-                    <i class="fa fa-square-o fa-stack-2x"></i>
-                    <i class="fa fa-google-plus fa-stack-1x"></i>
-                </span>
+            <a href="https://plus.google.com/+Setonescap" target="_blank" class="google-plus" title="Check out Set One's Cap at Google Plus">
+                <div class="fa-stack fa-2x">
+                    <span class="fa fa-square-o fa-stack-2x"></span>
+                    <span class="fa fa-google-plus fa-stack-1x"></span>
+                </div>
                 <p>Google+</p>
             </a>
 
         </div>
+
+        <div class="col-12">
+            <div class="line-through">
+                <span></span>
+
+                <h3>Subscribe to our newsletters</h3>
+            </div>
+            <form action="//setonescap.us11.list-manage.com/subscribe/post?u=aaa5751df5aedfb525de2a85c&amp;id=8d64514e26" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+
+                <div class="input-field-group">
+                    <input type="email" value="" name="EMAIL" class="required email input-field" id="mce-EMAIL" placeholder="Your email">
+                    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button bg-setonescap-red raised">
+                </div>
+                <div style="position: absolute; left: -5000px;">
+                    <input type="text" name="b_aaa5751df5aedfb525de2a85c_8d64514e26" tabindex="-1" value="">
+                </div>
+
+            </form>
+        </div>
+
+
         <div itemscope itemtype="http://data-vocabulary.org/Person" class="col-4">
             <h3>Web:</h3>
+
             <p><span itemprop="name">Benjamin Dehli</span></p>
+
             <div itemprop="address" itemscope itemtype="http://data-vocabulary.org/Address" class="no-padding">
                 <p><span itemprop="street-address">Margretes veg 15</span></p>
+
                 <p><span itemprop="postal-code">3804</span> <span itemprop="locality">B&oslash; i Telemark</span></p>
+
                 <p><span itemprop="country-name">Norway</span></p>
             </div>
             <p>Phone: +47 92 29 27 19</p>
+
             <p>E-mail: support@setonescap.com</p>
         </div>
         <div itemscope itemtype="http://data-vocabulary.org/Person" class="col-4">
-            <h3>Booking:</h3>
-            <p><span itemprop="name">Carl-Viktor Guttormsen</span></p>
-            <p>Parkveien 1</p>
-            <p>3970 Langesund</p>
-            <p>Norway</p>
-            <p>Phone: +47 90 26 21 60</p>
-            <p>E-mail: booking@setonescap.com</p>
+            <h3>Management / Booking:</h3>
+
+            <p><span itemprop="name">Jon F. Leiulfsrud</span></p>
+
+            <div itemprop="address" itemscope itemtype="http://data-vocabulary.org/Address" class="no-padding">
+                <p><span itemprop="street-address">Dokkveien 8</span></p>
+
+                <p><span itemprop="postal-code">3920 Porsgrunn</span></p>
+
+                <p><span itemprop="country-name">Norway</span></p>
+            </div>
+            <p>Phone: +47 90 10 92 84</p>
+
+            <p>E-mail: jon@compro.no</p>
         </div>
         <div class="col-4">
             <h3>For everything else:</h3>
@@ -158,13 +209,14 @@
 
 
     </div>
-    <footer class="fixed-bottom bg-white fg-black">
-        <div class="col-6">Copyright © 2014 <a href="https://plus.google.com/+Setonescap" target="_blank">Set One's Cap</a></div>
+    <footer class="bg-white fg-black">
+        <div class="col-6">© 2014
+            <a href="https://plus.google.com/+Setonescap" target="_blank" rel="publisher">Set One's Cap</a></div>
         <div class="col-6 text-right">
-            Website by: <a href="https://plus.google.com/+BenjaminDehli1/" rel="publisher" target="_blank">Benjamin Dehli</a>
+            Website by:
+            <a href="https://plus.google.com/+BenjaminDehli1/" target="_blank" rel="author">Benjamin Dehli</a>
         </div>
     </footer>
-
 
     <div class="clearfix"></div>
 </div>
@@ -172,7 +224,7 @@
 
 <script type="text/javascript" src="/scripts/jquery.slicknav.js"></script>
 <script type="text/javascript">
-    $(document).ready(function(){
+    $(document).ready(function () {
         $('#menu').slicknav();
     });
 </script>
