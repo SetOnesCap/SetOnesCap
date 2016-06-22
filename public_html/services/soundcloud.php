5903df4c3d2bc3fa6116b7b69ff839e0f29312df
Benjamin Dehli <superelg@gmail.com>
2016-06-23 00:55:27 +0200

    Add rich snippets for soundcloud audio elements

--- public_html/services/soundcloud.php
+++ public_html/services/soundcloud.php
@@ -9,8 +9,24 @@ $soundcloud_url = "http://api.soundcloud.com/users/" . $userid . "/tracks.json?c
 $tracks_json = getJson($soundcloud_url);
 $tracks = json_decode($tracks_json);
 
+function getDurationContent($trackDuration){
+    $input = $trackDuration;
+
+    $input = floor($input / 1000);
+
+    $seconds = $input % 60;
+    $input = floor($input / 60);
+
+    $minutes = $input % 60;
+
+    return "PT" . $minutes . "M" . $seconds . "S";
+}
+
 foreach ($tracks as $track) {
     $trackTitle = $track->title;
+    $trackDuration = $track->duration;
+    $trackDurationContent = getDurationContent($trackDuration);
+    $trackDurationSeconds = floor($trackDuration / 1000);
     $artworkUrl = $track->artwork_url;
     $artworkUrl = str_replace('large.jpg', 't500x500.jpg', $artworkUrl);
     $playbackCount = $track->playback_count;
@@ -22,14 +38,16 @@ foreach ($tracks as $track) {
 
     ?>
     <div class="col-4 soundcloud-feed" style="">
-        <div class="panel bg-white fg-black no-padding">
+        <div class="panel bg-white fg-black no-padding" itemscope itemtype="http://schema.org/MusicGroup">
+            <span style="display: none" itemprop="name">Set One's Cap</span>
             <div class="artwork-thumb">
 
-                <img src="<?php echo $artworkUrl; ?>" alt="Ocean Sound Recordings" class="">
+                <img src="<?php echo $artworkUrl; ?>" alt="Album cover for Set One's Cap - <?php echo $trackTitle; ?> " class="">
 
             </div>
-            <div class="trackinfo">
-                <h3><?php echo $trackTitle; ?>  </h3>
+            <div class="trackinfo" itemprop="tracks" itemscope itemtype="http://schema.org/MusicRecording">
+                <h3 itemprop="name"><?php echo $trackTitle; ?>  </h3>
+                <meta itemprop="duration" content="<?php echo $trackDurationContent ?>"/>
                 <audio controls>
                     <source src='<?php echo $streamLocation; ?>' type='audio/mp3'>
                     Your browser does not support the audio element.
@@ -39,8 +57,10 @@ foreach ($tracks as $track) {
                     <small><span class="fa fa-play"> </span> <?php echo $playbackCount; ?> plays</small>
                     <small><span class="fa fa-heart"> </span> <?php echo $likeCount; ?> likes</small>
                 </div>
+                <meta itemprop="interactionCount" content="UserPlays:<?php echo $playbackCount; ?>"/>
                 <div class="col-12">
-                    <a href="<?php echo $permalinkUrl; ?>" target="_blank" title="Listen to <?php echo $trackTitle; ?> at SoundCloud" class="button bg-setonescap-red"><span class="fa fa-soundcloud"></span> SoundCloud</a>
+                    <a href="<?php echo $permalinkUrl; ?>" target="_blank" itemprop="url" title="Listen to <?php echo $trackTitle; ?> at SoundCloud" class="button bg-setonescap-red"><span class="fa fa-soundcloud"></span> SoundCloud</a>
+                    <a href="http://soundcloud.com/setonescap/dont-you-wanna-know" itemprop="inAlbum" style="display: none"><?php echo $trackTitle; ?></a>
                 </div>
                 <div class="clearfix"></div>
             </div>
@@ -49,3 +69,4 @@ foreach ($tracks as $track) {
     <?php
 }
 ?>
+
